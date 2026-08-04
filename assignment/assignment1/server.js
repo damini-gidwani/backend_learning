8fc1229bf9c0ed7966dcf64d8121a6ad4b4c5cd0
Damini Gidwani <daminigidwani13@gmail.com>
2026-08-04 11:31:41 +0530

    created more api

--- assignment/assignment1/server.js
+++ assignment/assignment1/server.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const app = express();
 const connectDB = require("./db");
-const userModel = require("./userModel");
+const { userModel, productModel } = require("./userModel");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const joi = require("joi");
-const authMiddleware=require("./authMiddleware")
+const authMiddleware = require("./authMiddleware");
 const cookieParser = require("cookie-parser");
+const { RiReservedFill } = require("react-icons/ri");
 
 app.use(express.json());
 app.use(cookieParser());
 
 app.post("/register", async (req, res) => {
   try {
-    const validateSchema = joi.object({ 
+    const validateSchema = joi.object({
       fname: joi.string().min(2).max(128).required(),
       lname: joi.string().min(2).max(128).required(),
       dob: joi.date().required(),
@@ -66,7 +67,6 @@ app.post("/register", async (req, res) => {
 
 app.post("/login", async (req, res) => {
   try {
-    console.log(req.cookies);
     const validationSchema = joi.object({
       email: joi.string().email().required(),
       password: joi.string().min(8).required(),
@@ -97,7 +97,7 @@ app.post("/login", async (req, res) => {
       .send(`Welcome ${userExist.fname}!! You are logged in successfully!`);
   } catch (err) {
     console.log(err);
-    res.status(500).send("SERVER ERROR!");
+    res.status(500).send({ message: "Internal server error" });
   }
 });
 
@@ -105,6 +105,114 @@ app.get("/logout", authMiddleware, async (req, res) => {
   try {
     res.clearCookie("givenToken", { httpOnly: true });
     res.send("Logged out successfully!!");
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ message: "Internal server error" });
+  }
+});
+
+app.post("/createProduct", authMiddleware, async (req, res) => {
+  try {
+    const validateSchema = joi.object({
+      name: joi.string().required().min(2),
+      price: joi.number().required().min(0),
+      category: joi
+        .string()
+        .required()
+        .valid("electronics", "clothing", "books", "home", "sports"),
+      SKU: joi.string().required(),
+    });
+    const { error } = validateSchema.validate(req.body);
+    if (error) {
+      return res.status(400).json(error.details[0].message);
+    }
+    const { name, price, category, SKU } = req.body;
+    const product = {
+      name,
+      price,
+      category,
+      SKU,
+    };
+    let productExist = await productModel.findOne({ SKU });
+    if (productExist) {
+      return res.status(400).send({ message: "Product already exist!" });
+    }
+    const newproduct = await productModel.create(product);
+    res.status(201).json({
+      message: "Product created successfully",
+      product: newproduct,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ meassage: "Internal Server Error" });
+  }
+});
+
+app.delete("/deleteProductBySku/:sku", authMiddleware, async (req, res) => {
+  const sku = req.params.sku;
+  if (!sku) {
+    return res.status(400).json({ message: "give sku to delete product!" });
+  }
+  const isExist = await productModel.findOne({ SKU: sku });
+  if (!isExist) {
+    return res.status(400).send({ message: "Product does not exist!" });
+  }
+  await productModel.deleteOne({ SKU: sku });
+  res.status(200).json({ message: "product deleted successfully!!" });
+});
+
+app.get("/getAllProducts", authMiddleware, async (req, res) => {
+  try {
+    const { page = 1, limit = 2, sort = "asc" } = req.query;
+    const allProducts = await productModel
+      .find({})
+      .limit(Number(limit))
+      .skip((Number(page) - 1) * Number(limit))
+      .sort({ price: sort === "asc" ? 1 : -1 })
+      .select("-_id -SKU -__v");
+    res.json({ meassage: "All products : ", allProducts });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ meassage: "Internal Server Error" });
+  }
+});
+
+app.put("/updateProductById/:id", authMiddleware, async (req, res) => {
+  try {
+    const id = req.params.id;
+    if (!id) {
+      return res.status(400).json({ message: "give id to delete product!" });
+    }
+    const updateProduct = await productModel.findOneAndUpdate(
+      { _id: id },
+      {
+        $set: req.body,
+      },
+      { returnDocument: "after" },
+    );
+    if (!updateProduct) {
+      return res.status(404).json({ message: "product not found!" });
+    }
+    res.json({ message: "product update", updateProduct });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ meassage: "Internal Server Error" });
+  }
+});
+
+app.get("/getProductById/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    if (!id) {
+      return res.status(400).json({ message: "give id to delete product!" });
+    }
+    const product = await productModel
+      .findOne({ _id: id })
+      .select("-SKU -_id -__v");
+    if (!product) {
+      return res.status(400).send({ message: "Product does not exist!" });
+    }
+    res.json({ product });
   } catch (err) {
     console.log(err);
     res.status(500).send("SERVER ERROR!");
--- assignment/assignment1/userModel.js
+++ assignment/assignment1/userModel.js
@@ -13,30 +13,60 @@ const userSchema = new mongoose.Schema({
     required: true,
     match: /^[a-zA-Z]+$/,
   },
-  dob:{
-    type:Date,
-    trim:true,
-    required:true
+  dob: {
+    type: Date,
+    trim: true,
+    required: true,
   },
-  gen:{
-    type:String,
-    enum:["male","female","others"],
-    required:true,
-    trim:true
+  gen: {
+    type: String,
+    enum: ["male", "female", "others"],
+    required: true,
+    trim: true,
   },
-  email:{
-    type:String,
-    trim:true,
-    required:true,
-    unique:true,
-    match:/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true,
+    match: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
   },
-  password:{
-    type:String,
-    trim:true,
-    required:true,
-    minlength:8,
+  password: {
+    type: String,
+    trim: true,
+    required: true,
+    minlength: 8,
   },
 });
-const userModel=mongoose.model("user",userSchema);
-module.exports=userModel;
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+    match: /^[a-zA-Z ]+$/,
+  },
+  price: {
+    type: Number,
+    trim: true,
+    required: true,
+    min: 0,
+  },
+  category: {
+    type: String,
+    required: true,
+    trim: true,
+    enum: ["electronics", "clothing", "books", "home", "sports"],
+    lowercase: true,
+  },
+  SKU: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+});
+
+const userModel = mongoose.model("user", userSchema);
+const productModel = mongoose.model("product", productSchema);
+module.exports = { userModel, productModel };
